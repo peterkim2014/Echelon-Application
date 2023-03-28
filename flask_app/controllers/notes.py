1bfe0969f37d085e100fc4bfe7c9b7ad7c43938a
peterkim2014 <peterkim2014@gmail.com>
2023-03-28 18:09:33 -0400

    added notes controller w/ model

---
+++ flask_app/controllers/notes.py
@@ -0,0 +1,141 @@
+from flask import render_template, redirect, request, flash, session
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.note import Note
+
+@app.route("/homepage")
+def homepage():
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+
+    user_id = session["user_id"]
+    user = User.get_by_id(user_id)
+    all_notes = Note.get_all()
+
+    return render_template("home_page.html", all_notes=all_notes, user=user)
+
+@app.route("/view/<int:id>")
+def view_note(id):
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+    user = User.get_by_id(session["user_id"])
+
+
+    return render_template("view_note.html", user=user)
+
+@app.route("/note/new")
+def create_note_page():
+    return render_template("add_note.html")
+
+@app.route("/note/new/create", methods=["POST"])
+def create_note_form():
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+
+    data = {
+        "name": request.form["name"],
+        "description": request.form["description"],
+        "instruction": request.form["instruction"],
+        "duration": request.form["duration"],
+        "user_id": session["user_id"]
+    }
+    if Note.validate_recipe(data):
+        Note.save(data)
+        return redirect("/homepage")
+    else:
+        return redirect("/homepage")
+
+@app.route("/edit/<int:id>")
+def edit_recipe_page(id):
+    recipe = Note.get_by_id(id)
+    return render_template("edit_recipe.html", recipe=recipe)
+
+@app.route("/edit/recipe", methods=["POST"])
+def edit_recipe():
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+    data = {
+        "id": request.form["id"],
+        "name": request.form["name"],
+        "description": request.form["description"],
+        "instruction": request.form["instruction"],
+        "duration": request.form["duration"],
+        "user_id": session["user_id"]
+    }
+    
+    if Recipe.validate_recipe(data):
+        Recipe.update_recipe(data)
+        return redirect("/homepage")
+    else:
+        return redirect("/homepage")
+
+    # recipe = Recipe.get_by_id(request.form["id"])
+    # if not recipe or recipe.user_id != session["user_id"]:
+    #     return redirect("/homepage")
+    
+@app.route("/delete/<int:id>")
+def delete_recipe(id):
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+    Recipe.delete(id)
+    return redirect("/homepage")
+
+
+# cant like and unlike
+# how to get rid of duplicates (line 93-94)
+@app.route("/like/<int:id>")
+def like_recipe(id):
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+    data = {
+                "user_id": session["user_id"],
+                "recipe_id": id
+            }
+
+    is_liked = Recipe.get_many_id(session["user_id"])
+    if is_liked:
+        for recipe in is_liked:
+            print(recipe.id)
+            print(id)
+            if Recipe.check_like(data) == False:
+                print("LIKED")
+                Recipe.like(data)
+                return redirect("/homepage")
+            else:
+                print("ALREADY LIKED")
+    else:
+        print("Added to DB")
+        Recipe.like(data)
+
+
+    # if Recipe.get_many_id(session["user_id"]) == None:
+    #     data = {
+    #         "user_id": session["user_id"],
+    #         "recipe_id": id
+    #     }
+    #     Recipe.like(data)
+    #     print("liked")
+    #     return redirect("/homepage")
+    return redirect("/homepage")
+    
+
+@app.route("/unlike/<int:id>")
+def unlike_recipe(id):
+    if not "user_id" in session:
+        flash("Please Log In", "login")
+        return redirect("/login_page")
+    data = {
+        "user_id": session["user_id"],
+        "recipe_id": id
+    }
+    if Recipe.get_one_with_likes(session["user_id"]) != None:
+        Recipe.unlike(data)
+        print("unliked")
+        return redirect("/homepage")
+    return redirect("/homepage")
---
+++ flask_app/models/note.py
@@ -0,0 +1,83 @@
+from flask_app.config.mysqlconnection import MySQLConnection
+from flask import flash, session
+from flask_app.models.user import User
+
+class Note:
+    
+    dB = "echelon_data"
+
+    def __init__(self, notes_data):
+        self.id = notes_data["id"]
+        self.note = notes_data["note"]
+        self.created_at = notes_data["created_at"]
+        self.updated_at = notes_data["updated_at"]
+        self.user_id = notes_data["user_id"]
+        self.creator = None
+
+    @classmethod
+    def save(cls, data):
+        query = """
+            INSERT INTO notes (note, user_id) VALUES (%(note)s, %(user_id)s);
+        """
+        result = MySQLConnection(cls.dB).query_db(query, data)
+        return result
+    
+    @classmethod
+    def create(cls, data):
+        cls.save(data)
+        return data
+    
+    @classmethod
+    def get_all_notes_no_user(cls):
+        query = """
+            SELECT * FROM notes;
+        """
+        result = MySQLConnection(cls.dB).query_db(query)
+        return result
+    
+    @classmethod
+    def get_all_note_one_user(cls, id):
+        from flask_app.models.user import User
+        query = """
+            SELECT * FROM notes LEFT JOIN users ON notes.user_id = users.id WHERE notes.user_id = %(id)s;
+        """
+        results = MySQLConnection(cls.dB).query_db(query, {"id": id})
+        if results:
+            notes_list = []
+            for result in results:
+                user_data = {
+                    "id": result["id"],
+                    "username": result["username"],
+                    "email": result["email"],
+                    "password": None
+                }
+                creator = User(user_data)
+                note = cls(result)
+                note.creator = creator
+                notes_list.append(note)
+            return notes_list
+        return None
+
+    
+    @classmethod
+    def get_one_note(cls, id):
+        query = """
+            SELECT * FROM notes WHERE id = %(id)s;
+        """
+        result = MySQLConnection(cls.dB).query_db(query, {"id": id})
+        return cls(result[0] if result else None)
+    
+    @classmethod
+    def delete(cls, id):
+        query = """
+            DELETE FROM notes WHERE id = %(id)s;
+        """
+        result = MySQLConnection(cls.dB).query_db(query, {"id": id})
+        return result
+    
+    @staticmethod
+    def validate_recipe(data):
+        is_valid = True
+        return is_valid, data 
+
+ 
