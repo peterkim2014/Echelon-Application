9706ff954507e7074fd92f0fabfbc66223659ed9
peterkim2014 <peterkim2014@gmail.com>
2023-04-05 01:18:17 -0400

    added css

---
+++ flask_app/controllers/abouts.py
@@ -0,0 +1,11 @@
+from flask import render_template, redirect, request, flash, session
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.note import Note
+from flask_app.models.calender import Calender
+from flask_app.models.command import Command
+from flask_app.models.chat import Chat
+
+@app.route("/about_page")
+def about_page():
+    return render_template("main/about_page.html")
